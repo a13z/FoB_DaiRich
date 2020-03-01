94c09f088be03620c16d4a777e52b26f3329a956
Alfonso Gonzalez <alfonsog@gmail.com>
2020-03-01 09:01:17 +0000

    Deployment configuration

--- buidler.config.js
+++ buidler.config.js
@@ -1,4 +1,7 @@
 usePlugin("@nomiclabs/buidler-truffle5");
+usePlugin("@nomiclabs/buidler-waffle");
+usePlugin("@nomiclabs/buidler-etherscan");
+usePlugin("buidler-typechain");
 
 // This is a sample Buidler task. To learn how to create your own go to
 // https://buidler.dev/guides/create-task.html
@@ -10,4 +13,18 @@ task("accounts", "Prints the list of accounts", async () => {
   }
 });
 
-module.exports = {};
+const INFURA_API_KEY = "";
+const ROPSTEN_PRIVATE_KEY = "";
+
+module.exports = {
+  defaultNetwork: "buidlerevm",
+  networks: {
+    rinkeby: {
+      url: `https://ropsten.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [ROPSTEN_PRIVATE_KEY]
+    }
+  },
+  solc: {
+    version: "0.6.2"
+  }
+};
