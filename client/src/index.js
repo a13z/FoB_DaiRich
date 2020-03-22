c029047323e36f2b2c46e2732bd60f226c45abaa
Alex A <halixand@gmail.com>
2020-03-22 16:23:38 -0700

    Add web3js

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Web3 from 'web3';
+
+var web3 = new Web3(Web3.givenProvider || 'ws://localhost:7545');
+console.log('/// web3 ///', web3);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
