0004b674e40d13b7d1513ae6b4ca6dee6e343281
imalexsq <halixand@gmail.com>
2020-03-22 18:39:09 -0700

    WIP - build account page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Nav />
+      <div
+        className="row"
+        style={{ height: '150px', backgroundColor: '#282368' }}
+      >
+        <div className="col-sm">
+          <h1 style={{ color: 'white', position: 'absolute', bottom: '0px' }}>
+            My Account
+          </h1>
+        </div>
+      </div>
+      <div
+        className="row"
+        style={{ height: '100px', backgroundColor: '#282368', color: 'white' }}
+      >
+        <div className="col">
+          <img
+            width="80px"
+            src="https://s3.amazonaws.com/uifaces/faces/twitter/samgrover/128.jpg"
+            class="img-thumbnail"
+          />
+        </div>
+        <div className="col">Connected</div>
+      </div>
+      <div className="row">
+        <div className="col-sm">Account Balance</div>
+        <div className="col-sm">Investments Balance</div>
+      </div>
+      <div className="row">
+        <div className="col-sm">Minimum Account Balance</div>
+        <div className="col-sm">Investment Threshold</div>
+      </div>
+      <div className="row">
+        <div className="col-sm">
+          Send Eth
+          <input placeholder="Account" />
+          <input placeholder="Recipient" />
+          <button>Send</button>
+        </div>
+      </div>
     </div>
   );
 }
