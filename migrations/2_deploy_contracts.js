6299496d828f31180312d3dd8c0ed03827202362
Alex A <halixand@gmail.com>
2020-03-26 20:11:39 -0700

    Remove yarn

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var HybridBank = artifacts.require('./HybridBank.sol');
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+	deployer.deploy(HybridBank);
 };
