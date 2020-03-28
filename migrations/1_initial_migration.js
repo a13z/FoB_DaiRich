02f22f4919acaab861a7c0b22e411f6357ffae0c
Alfonso Gonzalez <alfonsog@gmail.com>
2020-03-28 19:47:49 +0000

    Fixed merge typo

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const Migrations = artifacts.require("Migrations");
-=======
-var Migrations = artifacts.require("./Migrations.sol");
->>>>>>> 6299496d828f31180312d3dd8c0ed03827202362
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
