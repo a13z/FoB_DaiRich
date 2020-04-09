936d0de1d8436a4e309b969fc0f67bf18fd6f4e3
Alex A <halixand@gmail.com>
2020-04-08 22:35:08 -0700

    Implement approve and enroll functions

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import HybridBankContract from './contracts/HybridBank.json';
 import getWeb3 from './getWeb3';
 import Nav from './Nav';
-
+import IERC20 from './contracts/IERC20.json';
 import './App.css';
+
+const HYBRID_BANK_CONTRACT_ADDRESS =
+	'0xF1769060B1773b06780B3786D8cdd05f9B39b688';
 
 class App extends Component {
 	state = {
@@ -15,11 +18,16 @@ class App extends Component {
 		investmentThreshold: 0,
 		minBal: 0,
 		setInvestmentThreshold: 50,
-		setDepositAmount: 0,
-		setMinBal: 0,
+		setDepositAmount: '',
+		setMinBal: '',
 		web3: null,
 		sendTo: '',
 		sendAmount: '',
+		hide: false,
+		erc20: null,
+		disabledEnroll: true,
+		approveAmount: '',
+		loading: false,
 	};
 
 	componentDidMount = async () => {
@@ -33,14 +41,17 @@ class App extends Component {
 			// Get the contract instance.
 			const instance = new web3.eth.Contract(
 				HybridBankContract,
-				'0xA2400E598dC2A62d297249B6b0a188E54D4A42Ed'
+				HYBRID_BANK_CONTRACT_ADDRESS
 			);
-
+			const instance2 = new web3.eth.Contract(
+				IERC20,
+				'0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD'
+			);
 			// Set web3, accounts, and contract to the state, and then proceed with an
 			// example of interacting with the contract's methods.
 			this.setState(
-				{ web3, accounts, contract: instance },
-				this.getAccountStats
+				{ web3, accounts, contract: instance, erc20: instance2 },
+				this.isAccountEnrolled
 			);
 		} catch (error) {
 			// Catch any errors for any of the above operations.
@@ -76,14 +87,30 @@ class App extends Component {
 
 	setDepositAmount = async () => {
 		const { contract, accounts, setDepositAmount } = this.state;
+		this.setState({ loading: true });
 		await contract.methods
 			.deposit(setDepositAmount)
 			.send({ from: accounts[0] });
+		this.setState({ loading: false });
+		this.getAccountStats();
 	};
 
 	sendDai = async () => {
 		const { contract, accounts, sendTo, sendAmount } = this.state;
 		await contract.methods.pay(sendTo, sendAmount).send({ from: accounts[0] });
+	};
+
+	isAccountEnrolled = async () => {
+		const { contract, accounts } = this.state;
+		const enrolled = await contract.methods
+			.isAccountEnrolled()
+			.call({ from: accounts[0] });
+		if (enrolled) {
+			this.setState({ hide: false });
+			this.getAccountStats();
+		} else {
+			this.setState({ hide: true });
+		}
 	};
 
 	getAccountStats = async () => {
@@ -105,6 +132,30 @@ class App extends Component {
 		});
 	};
 
+	approve = async () => {
+		const { erc20, accounts, approveAmount } = this.state;
+		this.setState({ loading: true });
+		const erc20Res = await erc20.methods
+			.approve(HYBRID_BANK_CONTRACT_ADDRESS, approveAmount)
+			.send({ from: accounts[0] });
+		this.setState({ loading: false });
+
+		if (erc20Res.status) {
+			this.setState({ disabledEnroll: false });
+		}
+	};
+
+	enroll = async () => {
+		const { contract, accounts } = this.state;
+		this.setState({ loading: true });
+		const res = await contract.methods.enroll().send({ from: accounts[0] });
+		this.setState({ loading: false });
+
+		if (res.status) {
+			this.isAccountEnrolled();
+		}
+	};
+
 	render() {
 		if (!this.state.web3) {
 			return <div>Loading Web3, accounts, and contract...</div>;
@@ -112,175 +163,217 @@ class App extends Component {
 		return (
 			<div className='container'>
 				<Nav />
-				<div
-					className='row'
-					style={{ height: '150px', backgroundColor: '#282368' }}
-				>
-					<div className='col-sm'>
-						<h1 style={{ color: 'white', position: 'absolute', bottom: '0px' }}>
-							My Account
-						</h1>
+				<div style={{ display: this.state.hide ? 'block' : 'none' }}>
+					<div className='alert alert-danger' role='alert'>
+						You must enroll first!
+					</div>
+					<div className='row'>
+						<div className='col'>
+							<input
+								name='approveAmount'
+								type='number'
+								className='form-control'
+								value={this.state.approveAmount}
+								onChange={(e) => this.handleChange(e)}
+								placeholder='Enter amount to deposit into bank account'
+							/>
+							<br />
+							<button
+								type='button'
+								className='btn btn-success'
+								onClick={() => this.approve()}
+							>
+								{this.state.loading ? 'Processing...' : 'Approve'}
+							</button>
+						</div>
+					</div>
+					<div className='row pt-3'>
+						<div className='col'>
+							<button
+								type='button'
+								className='btn btn-success'
+								onClick={() => this.enroll()}
+								disabled={this.state.disabledEnroll}
+							>
+								{this.state.loading ? 'Processing...' : 'Enroll'}
+							</button>
+						</div>
 					</div>
 				</div>
-
-				<div
-					className='row'
-					style={{
-						height: '100px',
-						backgroundColor: '#282368',
-						color: 'white',
-					}}
-				>
-					<div className='col'>
+				<div style={{ display: this.state.hide ? 'none' : 'block' }}>
+					<div
+						className='row'
+						style={{ height: '150px', backgroundColor: '#282368' }}
+					>
+						<div className='col-sm'>
+							<h1
+								style={{ color: 'white', position: 'absolute', bottom: '0px' }}
+							>
+								My Account
+							</h1>
+						</div>
+					</div>
+					<div
+						className='row'
+						style={{
+							height: '100px',
+							backgroundColor: '#282368',
+							color: 'white',
+						}}
+					>
+						<div className='col'>
+							<div className='row row-cols-2'>
+								<div className='col'>
+									<img
+										width='80px'
+										src='https://s3.amazonaws.com/uifaces/faces/twitter/samgrover/128.jpg'
+										alt=''
+										className='rounded-circle'
+									/>
+								</div>
+								<div className='col-6'>
+									<p style={{ fontSize: '14px' }}>
+										John Doe
+										<br />
+										Chief Creative Officer
+									</p>
+								</div>
+							</div>
+						</div>
+						<div className='col'>
+							<button className='btn btn-warning' style={{ float: 'right' }}>
+								{this.state.web3.currentProvider.isConnected
+									? 'Connected'
+									: 'Not Connected'}
+							</button>
+						</div>
+					</div>
+					<div className='row row-cols-2'>
+						<div className='col-sm infoBox'>
+							<p>{this.state.balance}</p>
+							<p>Account Balance</p>
+						</div>
+						<div className='col-sm infoBox'>
+							<p>{this.state.invested}</p>
+							<p>Investments Balance</p>
+						</div>
+					</div>
+					<div className='row row-cols-2'>
+						<div className='col-sm infoBox'>
+							<p>{this.state.minBal}</p>
+							<p>Minimum Account Balance</p>
+						</div>
+						<div className='col-sm infoBox'>
+							<p>{this.state.investmentThreshold}</p>
+							<p>Investment Threshold</p>
+						</div>
+					</div>
+					<div className='row m-1'>
+						Send
 						<div className='row row-cols-2'>
-							<div className='col'>
-								<img
-									width='80px'
-									src='https://s3.amazonaws.com/uifaces/faces/twitter/samgrover/128.jpg'
-									alt=''
-									className='rounded-circle'
+							<div className='col-sm'>
+								<input
+									placeholder='Recipient'
+									className='form-control'
+									name='sendTo'
+									value={this.state.sendTo}
+									onChange={(e) => this.handleChange(e)}
 								/>
 							</div>
-							<div className='col-6'>
-								<p style={{ fontSize: '14px' }}>
-									John Doe
-									<br />
-									Chief Creative Officer
-								</p>
+							<div className='col-sm'>
+								<input
+									placeholder='Amount'
+									className='form-control'
+									name='sendAmount'
+									value={this.state.sendAmount}
+									onChange={(e) => this.handleChange(e)}
+								/>
 							</div>
 						</div>
-					</div>
-					<div className='col'>
-						<button className='btn btn-warning' style={{ float: 'right' }}>
-							{this.state.web3.currentProvider.isConnected
-								? 'Connected'
-								: 'Not Connected'}
+						<button
+							type='button'
+							className='btn btn-dark btn-block mt-1'
+							onClick={() => this.sendDai()}
+						>
+							{this.state.loading ? 'Processing...' : 'Send'}
 						</button>
 					</div>
-				</div>
-				<div className='row row-cols-2'>
-					<div className='col-sm infoBox'>
-						<p>{this.state.balance}</p>
-						<p>Account Balance</p>
-					</div>
-					<div className='col-sm infoBox'>
-						<p>{this.state.invested}</p>
-						<p>Investments Balance</p>
-					</div>
-				</div>
-				<div className='row row-cols-2'>
-					<div className='col-sm infoBox'>
-						<p>{this.state.minBal}</p>
-						<p>Minimum Account Balance</p>
-					</div>
-					<div className='col-sm infoBox'>
-						<p>{this.state.investmentThreshold}</p>
-						<p>Investment Threshold</p>
-					</div>
-				</div>
-				<div className='row m-1'>
-					Send
-					<div className='row row-cols-2'>
+					<div className='row m-1'>
+						Withdraw
 						<div className='col-sm'>
-							<input
-								placeholder='Recipient'
-								className='form-control'
-								name='sendTo'
-								value={this.state.sendTo}
-								onChange={(e) => this.handleChange(e)}
-							/>
+							<input placeholder='Amount' className='form-control' disabled />
 						</div>
+						<button
+							type='button'
+							className='btn btn-dark btn-block mt-1'
+							disabled
+						>
+							{this.state.loading ? 'Processing...' : 'Withdraw'}
+						</button>
+					</div>
+					<div className='row m-1'>
+						Deposit
 						<div className='col-sm'>
 							<input
 								placeholder='Amount'
 								className='form-control'
-								name='sendAmount'
-								value={this.state.sendAmount}
+								name='setDepositAmount'
+								onChange={(e) => this.handleChange(e)}
+								value={this.state.setDepositAmount}
+							/>
+						</div>
+						<button
+							type='button'
+							className='btn btn-dark btn-block mt-1'
+							onClick={() => this.setDepositAmount()}
+						>
+							{this.state.loading ? 'Processing...' : 'Deposit'}
+						</button>
+					</div>
+					<div className='row m-1'>
+						Set minimum balance
+						<div className='col-sm'>
+							<input
+								placeholder='Amount'
+								name='setMinBal'
+								className='form-control'
+								value={this.state.setMinBal}
 								onChange={(e) => this.handleChange(e)}
 							/>
 						</div>
+						<button
+							type='button'
+							className='btn btn-dark btn-block mt-1'
+							onClick={() => this.setMinBalance()}
+						>
+							{this.state.loading ? 'Processing...' : 'Set minimum balance'}
+						</button>
 					</div>
-					<button
-						type='button'
-						className='btn btn-dark btn-block mt-1'
-						onClick={() => this.sendDai()}
-					>
-						Send
-					</button>
-				</div>
-				<div className='row m-1'>
-					Withdraw
-					<div className='col-sm'>
-						<input placeholder='Amount' className='form-control' disabled />
+					<div className='row m-1'>
+						Set Investment threshold
+						<div className='col-sm'>
+							<label htmlFor='customRange1'>
+								{this.state.setInvestmentThreshold}
+							</label>
+							<input
+								type='range'
+								className='custom-range'
+								id='customRange1'
+								onChange={(e) =>
+									this.setState({ setInvestmentThreshold: e.target.value })
+								}
+							/>
+						</div>
+						<button
+							type='button'
+							className='btn btn-dark btn-block mt-1'
+							onClick={() => this.setInvestmentThreshold()}
+						>
+							{this.state.loading
+								? 'Processing...'
+								: 'Set investment threshold'}
+						</button>
 					</div>
-					<button
-						type='button'
-						className='btn btn-dark btn-block mt-1'
-						disabled
-					>
-						Withdraw
-					</button>
-				</div>
-				<div className='row m-1'>
-					Deposit
-					<div className='col-sm'>
-						<input
-							placeholder='Amount'
-							className='form-control'
-							name='setDepositAmount'
-							onChange={(e) => this.handleChange(e)}
-							value={this.state.setDepositAmount}
-						/>
-					</div>
-					<button
-						type='button'
-						className='btn btn-dark btn-block mt-1'
-						onClick={() => this.setDepositAmount()}
-					>
-						Deposit
-					</button>
-				</div>
-				<div className='row m-1'>
-					Set minimum balance
-					<div className='col-sm'>
-						<input
-							placeholder='Amount'
-							name='setMinBal'
-							className='form-control'
-							value={this.state.setMinBal}
-							onChange={(e) => this.handleChange(e)}
-						/>
-					</div>
-					<button
-						type='button'
-						className='btn btn-dark btn-block mt-1'
-						onClick={() => this.setMinBalance()}
-					>
-						Set minimum balance
-					</button>
-				</div>
-				<div className='row m-1'>
-					Set Investment threshold
-					<div className='col-sm'>
-						<label htmlFor='customRange1'>
-							{this.state.setInvestmentThreshold}
-						</label>
-						<input
-							type='range'
-							className='custom-range'
-							id='customRange1'
-							onClick={(e) =>
-								this.setState({ setInvestmentThreshold: e.target.value })
-							}
-						/>
-					</div>
-					<button
-						type='button'
-						className='btn btn-dark btn-block mt-1'
-						onClick={() => this.setInvestmentThreshold()}
-					>
-						Set investment threshold
-					</button>
 				</div>
 			</div>
 		);
--- client/src/Nav.js
+++ client/src/Nav.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Nav = () => (
 	<nav className='navbar navbar-expand-lg navbar-light bg-light'>
-		<a className='navbar-brand'>Navbar</a>
+		<a className='navbar-brand'>DaiRich</a>
 		<button
 			className='navbar-toggler'
 			type='button'
